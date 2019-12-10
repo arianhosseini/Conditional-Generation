8bc72734886ebe624840b89d658fd2a4dcf31974
Shawn Tan <shawn@wtf.sg>
2019-12-10 14:37:11 -0500

    Generated stuff (*Shawn)

--- run_generation.sh
+++ run_generation.sh
@@ -1,12 +1,12 @@
-for top_args in "--top_k 0 --top_p 1." "--top_k 8 --top_p 0.9"
+for top_args in "--top_k 0 --top_p 1." # "--top_k 8 --top_p 0.9"
 do
-    for LENGTH in 60 100
+    for LENGTH in 60 # 100
     do
-        for MODE in ltr rnd_perm rnd
+        for MODE in rnd #ltr rnd_perm 
         do
             for REF in gibbs xent none
             do
-                python -u run_generation.py \
+                echo sbatch --gres=gpu:1 --mem=4GB ./run.sh run_generation.py \
                     --model_type xlnet \
                     --model_name_or_path xlnet-base-cased \
                     --refine $REF \
